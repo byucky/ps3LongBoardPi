5371069583d80c4d15dcfa16e4cc168078c2bf0d
Aaron Light <aaron.light04@gmaili.com>
2017-03-16 20:58:06 -0600

    Added power off timeout with controller using O

---
+++ piboard.py
@@ -0,0 +1,106 @@
+import pygame
+import time
+import os
+#import pigpio
+import time
+import sys
+import threading
+import subprocess
+
+pathToController = '/dev/input/js0'
+
+INPUTS = {
+    "THROTTLE_AXIS" : 1,
+    "THROTTLE_ENABLE": 8,
+    "POWER_OFF": 13,
+    "CRUISE" : 10, 
+}
+
+is_debug = "debug" in sys.argv
+
+power_off = 0
+
+def PowerOffPi():
+    elapsed_time = 0
+    while(elapsed_time < 5):
+        time.sleep(1)
+        if power_off is 1:
+            elapsed_time += 1
+        else:
+            elapsed_time = 0
+    # power off pi
+    print("power off pi")
+
+class Skateboard():
+
+    def __init__(self):
+        self.powerThread = threading.Thread(target=PowerOffPi)
+        self.buttons = {
+            'axis' : 0,
+            'enable': 0,
+            'power_off': 0
+        }
+
+    def getInput(self):
+        # get input from controllers
+        events = pygame.event.get()
+        changes = {}
+        if events is not None:
+            for event in events:
+                if event.type == pygame.JOYAXISMOTION:
+                    if event.axis == INPUTS.THROTTLE_AXIS:
+                        changes["axis"] = event.value
+                if event.type == pygame.JOYBUTTONDOWN:
+                    if event.button == INPUTS.THROTTLE_ENABLE:
+                        changes["enable"] = 1
+                    if event.button == INPUTS.POWER_OFF:
+                        changes['power_off'] = 1
+                if event.type == pygame.JOYBUTTONUP:
+                    if event.button == INPUTS.THROTTLE_ENABLE:
+                        changes["enable"] = 0
+                    if event.button == INPUTS.POWER_OFF:
+                        changes['power_off'] = 0
+        return changes
+
+    def update(self, changes):
+        #update state based on gathered input
+        if axis in changes:
+            # self.buttons['axis'] = changes['axis']
+            self.updateSpeed(changes['axis'])
+        if enable in changes:
+            self.buttons['enable'] = changes['enable']
+        if power in changes:
+            self.buttons['power_off'] = changes['power_off']
+            self.adjustPowerOff()
+            
+    def adjustPowerOff():
+        if self.buttons['power_off'] is 1:
+            power_off = 1
+        elif self.buttons['power_off'] is 0:
+            power_off = 0
+
+    def updateSpeed(self, newSpeed):
+        return True
+    
+    def OutputButtonValues(self):
+        print("throttle: " + str(round(self.buttons['axis'],2)))
+        print("enable : " + str(self.buttons['enable']))
+
+    def mainloop(self):
+        while(True):
+            self.getInput()
+            self.update(btns)
+            if(is_debug):
+                self.OutputButtonValues()
+            time.sleep(0.1)
+
+
+    
+
+
+def main():
+    skate = Skateboard()
+    
+
+if __name__ == "__main__":
+	main()
